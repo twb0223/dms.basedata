c0efdc3f7918ac62b83635a256166c7d48bacf75
twb0223 <twb-008@163.com>
2015-06-26 11:56:39 +0800

    add

--- DMS.BaseData/BaseData.Web/Common/Tools.cs
+++ DMS.BaseData/BaseData.Web/Common/Tools.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Web;
@@ -56,6 +57,69 @@ namespace BaseData.Web.Common
             string result = BitConverter.ToString(md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes(strText)), 4, 8);
             result = result.Replace("-", "");
             return result;
+        }
+
+        /// <summary>
+        /// Post请求
+        /// </summary>
+        /// <param name="postUrl"></param>
+        /// <param name="paramData"></param>
+        /// <param name="dataEncode"></param>
+        /// <returns></returns>
+        public static string PostWebRequest(string postUrl, string paramData, Encoding dataEncode)
+        {
+            string ret = string.Empty;
+            try
+            {
+                byte[] byteArray = dataEncode.GetBytes(paramData); //转化
+                HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(new Uri(postUrl));
+                webReq.Method = "POST";
+                webReq.ContentType = "application/json";
+                webReq.ContentLength = byteArray.Length;
+                Stream newStream = webReq.GetRequestStream();
+                newStream.Write(byteArray, 0, byteArray.Length);//写入参数
+                newStream.Close();
+                HttpWebResponse response = (HttpWebResponse)webReq.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream(), dataEncode);
+                ret = sr.ReadToEnd();
+                sr.Close();
+                response.Close();
+                newStream.Close();
+            }
+            catch (Exception ex)
+            {
+                ret = ex.Message;
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// GET请求
+        /// </summary>
+        /// <param name="Url">URL</param>
+        /// <param name="postDataStr">请求参数</param>
+        /// <param name="type">Contenttype</param>
+        /// <returns></returns>
+        public static string GetWebRequest(string Url, string postDataStr, string type, Encoding dataEncode)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url + (postDataStr == "" ? "" : "?") + postDataStr);
+            request.Method = "GET";
+            if (type == "json")
+            {
+                request.ContentType = "application/json;charset=UTF-8";
+            }
+            else
+            {
+                request.ContentType = "text/html;charset=UTF-8";
+            }
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, dataEncode);
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            return retString;
+
         }
     }
 }
